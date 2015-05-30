b2172b4f20b18ee94074e32d8120688960efb203
Shahin <shahin.y@freenet.de>
2015-05-30 16:06:02 +0200

    Testweise Einbindung von H2

--- MeinBudgetTeam4/src/BudgetPlan.java
+++ MeinBudgetTeam4/src/BudgetPlan.java
@@ -7,6 +7,8 @@ public class BudgetPlan {
 	public static void main(String[] args) {
 		BudgetPlanModel budget = new BudgetPlanModel(); // Modell
 		new BudgetPlanGUI(budget); // View und Controller
+		BudgetPlanModel test = new BudgetPlanModel();
+		test.testFunktion();
 	}
 
 }
--- MeinBudgetTeam4/src/BudgetPlanModel.java
+++ MeinBudgetTeam4/src/BudgetPlanModel.java
@@ -14,9 +14,65 @@ import com.opencsv.CSVReader;
  * Datenmodell des Budgetplaners
  * 
  * Die Daten werden in der Datei data/budget.csv abgespeichert als CSV-Datei.
+ * Bzw. in unserem Fall über H2 in einer lokalen Datenbank.
  * 
  */
-public class BudgetPlanModel {
+
+
+import java.sql.*;
+import java.sql.*;
+/**
+ * Hello world!
+ *
+ */
+public class BudgetPlanModel
+{
+	public BudgetPlanModel() { // Konstruktor
+		
+	}
+    public void testFunktion()
+    {
+        try {
+            Class.forName("org.h2.Driver");
+            Connection dbConnection = DriverManager.getConnection("jdbc:h2:~/h2testdb");
+            Statement statement = dbConnection.createStatement();
+            statement.execute("DROP TABLE IF EXISTS Test;");
+            statement.close();
+            statement = dbConnection.createStatement();
+            statement.executeUpdate("CREATE TABLE Test (Test_ID INTEGER, Message VARCHAR(250));");
+            statement.close();
+
+            statement = dbConnection.createStatement();
+            statement.executeUpdate("INSERT INTO Test VALUES (4711, 'Hello World!');");
+            statement.close();
+            
+            statement = dbConnection.createStatement();
+            statement.executeUpdate("INSERT INTO Test VALUES (1, 'Hier könnten Daten aus dem Budgetplaner stehen!');");
+            statement.close();
+
+            statement = dbConnection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Test;");
+            while (resultSet.next()) {
+                String msg = resultSet.getString("Message");
+                System.out.println(msg);
+            }
+            statement.close();
+
+            dbConnection.close();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Unable to load the driver class!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+/*public class BudgetPlanModel {
 	List<Posten> ausgaben;
 
 	public BudgetPlanModel() {
@@ -48,4 +104,4 @@ public class BudgetPlanModel {
 			System.exit(1);
 		}
 	}
-}
+} */
