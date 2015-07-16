45dd87539a46cbd2a27af00426f633d81ca2adfd
Shahin <shahin.y@freenet.de>
2015-07-16 17:17:22 +0200

    Umbenannt

---
+++ MeinBudgetTeam4/src/AlertView.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class AlertView {
+
+	
+	public void startUp(Exception exc) {
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("AlertDialog.fxml"));
+		Stage primaryStage = new Stage(StageStyle.UNDECORATED);
+		try {
+			primaryStage.setScene(new Scene((Pane) loader.load()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		primaryStage.setTitle("Liquide wie Friede - Fehler");
+		AlertController alertC = loader.<AlertController>getController();
+		alertC.errorOcurred(exc);
+		primaryStage.setResizable(false);
+		primaryStage.getIcons().add(new Image("file:data/Coin.png"));
+		primaryStage.show();
+		
+	}
+}
